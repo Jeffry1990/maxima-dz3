901539a8bc92919b4cb903d690f2b4c1c2d839ae
Jeffry1990 <109137757+Jeffry1990@users.noreply.github.com>
2024-12-01 15:31:42 +0300

    Add files via upload

---
+++ ConsoleApp40/Program.cs
@@ -0,0 +1,16 @@
+﻿int enteredNumber;
+int reversedNumber = 0;
+int remainder;
+
+Console.Write("Введите число: ");
+enteredNumber = Convert.ToInt32(Console.ReadLine());
+
+while (enteredNumber != 0)
+{
+    remainder = enteredNumber % 10;
+    reversedNumber = reversedNumber * 10 + remainder;
+    enteredNumber /= 10;
+}
+
+Console.WriteLine($"Обратное число = {reversedNumber}");
+Console.ReadLine();
